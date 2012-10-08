2fe669db0c7be4ea09a5ed7e391959e60ddb0071
David <mingchang716@gmail.com>
2012-10-07 22:37:03 -0500

    initial commit for confluence plugin skeleton

---
+++ src/test/java/com/herzum/confluence/plugins/ExampleMacroTest.java
@@ -0,0 +1,15 @@
+package com.herzum.confluence.plugins;
+
+import org.junit.Test;
+
+/**
+ * Testing {@link com.herzum.confluence.plugins.ExampleMacro}
+ */
+public class ExampleMacroTest
+{
+    @Test
+    public void basic()
+    {
+        // add test here...
+    }
+}
